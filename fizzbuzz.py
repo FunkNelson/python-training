f5a5b41003158390cbc22190573c6773b943527d
Morgan Ross <rossmb@gmail.comm>
2025-10-14 16:07:02 -0700

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,22 @@
+# if divisible by 3, print "Fizz"
+# if divisible by 5, print "Buzz"
+# if divisible by both 3 and 5, print "FizzBuzz"
+# if not divisible by either, print the number
+
+def fizz_buzz(input):
+    answer = ""
+    if input % 3 == 0:
+        answer += "Fizz"
+    if input % 5 == 0:
+        answer += "Buzz"
+
+    if answer:
+        return answer
+    else:
+        return input
+
+
+print(fizz_buzz(12))
+print(fizz_buzz(25))
+print(fizz_buzz(30))
+print(fizz_buzz(7))
--- mathdoodle.py
+++ mathdoodle.py
@@ -2,9 +2,4 @@ def multiply(*numbers):
     product = 1
     for i in numbers:
         product *= i
-    return product
-
-
-print(multiply(2, 3, 4))  # Outputs: 24
-print(multiply(5, 6))     # Outputs: 30
-print(multiply(7))        # Outputs: 7
+        return product
